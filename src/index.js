b85b368ae4a7b75f06c3ca462c62a78dcfc843d5
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2022-02-20 00:50:26 +0530

    Creating the Store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+// Creating Store and Passing reducer to the store
+const store = createStore(movies);
+console.log("STATE", store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
-export default function movies(state = [], action) {
+// Creating the reducer, which gets the new state as first argumnet, and action as second argument.
+export default function movies(state = {}, action) {
     if (action.type === 'ADD_MOVIES') {
         return action.movies;
     }
