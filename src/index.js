4d333cdf77633325791f73172eb34fd4ddeafa2a
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2022-02-20 11:02:36 +0530

    Updating Comments for understanding

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 import App from './components/App';
 import movies from './reducers';
 
-// Creating Store and Passing reducer to the store
+// Creating Store and Passing reducer to the store, reducer gets triggered whenever a new action is dispatched to the store.
 const store = createStore(movies);
 console.log("BEFORE STATE", store.getState());
 
+//Dispatching action of ADD_MOVIES to the store , which gets passed to the reducer, and then reducer performs some activity based on the type of action and then return the new state to the store. Then Store replaces its state with the new state sent by the reducer.
 store.dispatch({
   type: 'ADD_MOVIES',
   movies: [{ name: "Superman" }]
@@ -19,7 +20,7 @@ console.log("AFTER STATE", store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
-// Creating the reducer, which gets the new state as first argumnet, and action as second argument.
+// Creating the reducer, which gets the new state as first argumnet, and action as second argument. And based on the action type, we return the newState to the store.
+//Here if the action type is ADD_MOVIES then we return the movies to the state, and it becomes the value of the state.
 export default function movies(state, action) {
     if (action.type === 'ADD_MOVIES') {
         return action.movies;
